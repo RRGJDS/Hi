e123e8c053fabf60580ef3a0ef2956e0be0bed30
RRGJDS <99534847+RRGJDS@users.noreply.github.com>
2022-03-02 00:07:23 +0900

    Update JJ.js

--- JJ.js
+++ JJ.js
@@ -11,4 +11,8 @@ var normal = new Audio("audio/Normal ending.mp3");
 var truth = new Audio("audio/truth.mp3");
 var battle = new Audio("audio/battle.mp3");
 
-
+document.querySelector("#audioP").addEventListener("click", function(){
+    happy.play();
+    happy.loop = true;
+    happy.volume = 0.2;
+});
