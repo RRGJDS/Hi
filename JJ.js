3e57b7f8a070ebcc47bc82b71b4f1b58cd5c4098
RRGJDS <99534847+RRGJDS@users.noreply.github.com>
2022-03-01 23:38:19 +0900

    Add files via upload

--- JJ.js
+++ JJ.js
@@ -5,3 +5,12 @@ var SKYlive = true;
 var pudding = true;
 
 var suspect = true;
+
+var happy = new Audio("audio/happy ending.mp3");
+var normal = new Audio("audio/Normal ending.mp3");
+var truth = new Audio("audio/truth.mp3");
+var battle = new Audio("audio/battle.mp3");
+
+happy.play();
+happy.loop = true;
+happy.volume = 0.2;
