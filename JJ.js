1d5ce3634f4a420247d033237ab733e4acd42d58
RRGJDS <99534847+RRGJDS@users.noreply.github.com>
2022-03-09 21:33:06 +0900

    Add files via upload

--- JJ.js
+++ JJ.js
@@ -9,25 +9,25 @@
 // }
 
 
-// function setCookie(key, value, expiredays) {
-//   let todayDate = new Date();
-//   todayDate.setDate(todayDate.getDate() + expiredays); // 현재 시각 + 일 단위로 쿠키 만료 날짜 변경
-//   //todayDate.setTime(todayDate.getTime() + (expiredays * 24 * 60 * 60 * 1000)); // 밀리세컨드 단위로 쿠키 만료 날짜 변경
-//   document.cookie = key + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";";
-// }
+function setCookie(key, value, expiredays) {
+  let todayDate = new Date();
+  todayDate.setDate(todayDate.getDate() + expiredays); // 현재 시각 + 일 단위로 쿠키 만료 날짜 변경
+  //todayDate.setTime(todayDate.getTime() + (expiredays * 24 * 60 * 60 * 1000)); // 밀리세컨드 단위로 쿠키 만료 날짜 변경
+  document.cookie = key + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";";
+}
 
-// function getCookie(key){
-//   key = new RegExp(key + '=([^;]*)');
-//   return key.test(document.cookie) ? unescape(RegExp.$1) : '';}
+function getCookie(key){
+  key = new RegExp(key + '=([^;]*)');
+  return key.test(document.cookie) ? unescape(RegExp.$1) : '';}
 
-// function reset(){  
-//   setCookie('HJlive', '3', '2');
-//   setCookie('SKYlive', '3', '2');
-//   setCookie('pudding', '3', '2');
-//   setCookie('suspect', '3', '2');
-//   setCookie('Dio', '0', '2');
-//   setCookie('a', '1', '1');
-// }
+function reset(){  
+  setCookie('HJlive', '3', '2');
+  setCookie('SKYlive', '3', '2');
+  setCookie('pudding', '3', '2');
+  setCookie('suspect', '3', '2');
+  setCookie('Dio', '0', '2');
+  setCookie('a', '1', '1');
+}
 
 
 // var happy = new Audio("audio/happy ending.mp3");
