528a3ae143061c54192c1a4cd6d607c1faf97972
RRGJDS <99534847+RRGJDS@users.noreply.github.com>
2022-03-09 18:33:02 +0900

    Add files via upload

--- JJ.js
+++ JJ.js
@@ -7,6 +7,27 @@
 
 //     var suspect = true;
 // }
+
+function setCookie(key, value, expiredays) {
+  let todayDate = new Date();
+  todayDate.setDate(todayDate.getDate() + expiredays); // 현재 시각 + 일 단위로 쿠키 만료 날짜 변경
+  //todayDate.setTime(todayDate.getTime() + (expiredays * 24 * 60 * 60 * 1000)); // 밀리세컨드 단위로 쿠키 만료 날짜 변경
+  document.cookie = key + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";";
+}
+
+function getCookie(key){
+  key = new RegExp(key + '=([^;]*)');
+  return key.test(document.cookie) ? unescape(RegExp.$1) : '';}
+
+function reset(){  
+  setCookie('HJlive', '3', '2');
+  setCookie('SKYlive', '3', '2');
+  setCookie('pudding', '3', '2');
+  setCookie('suspect', '3', '2');
+  setCookie('Dio', '0', '2');
+  setCookie('a', '1', '1');
+}
+
 var happy = new Audio("audio/happy ending.mp3");
 var normal = new Audio("audio/Normal ending.mp3");
 var truth = new Audio("audio/truth.mp3");
@@ -19,4 +40,3 @@ document.querySelector("#audioP").addEventListener("click", function(){
     happy.currentTime = 0;
 });
 
-Cookies.set("Test","12");
